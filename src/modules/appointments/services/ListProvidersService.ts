45614a5f94aa640ac4d629f67051d1e8e4e36037
luanbrandao <luanbrandao4@gmail.com>
2020-07-02 21:34:04 -0300

    Cache lista de providersCache e segurança

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -11,12 +12,27 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProvider({
-      except_user_id: user_id,
-    });
+    // verifica se existe dados no cache
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProvider({
+        except_user_id: user_id,
+      });
+    }
+
+    console.log('A query di banco foi feita');
+
+    // providers-list:${user_id}, um cache para cada usuário
+    await this.cacheProvider.save(`providers-list:${user_id}`, users);
 
     return users;
   }
